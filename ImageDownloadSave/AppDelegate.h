1ce397905235bad292d821b6b55b7ae3cff68881
shawnDu <shuaishuai.du@smartereye.com>
2015-08-12 18:54:52 +0800

    Initial Commit

---
+++ ImageDownloadSave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageDownloadSave
+//
+//  Created by ShawnDu on 15/8/12.
+//  Copyright (c) 2015年 ShawnDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageDownloadSave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageDownloadSave
+//
+//  Created by ShawnDu on 15/8/12.
+//  Copyright (c) 2015年 ShawnDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
